9ee6ffd23a26346d6f04c0952ed363d5139a88ab
Mounika Potturu <mounika.10669270@ltimindtree.com>
2023-09-09 17:50:36 +0530

    Resource creation with SG

--- main_ec2.tf
+++ main_ec2.tf
@@ -3,8 +3,7 @@ resource "aws_instance" "EC2-Intance" {
   ami           = "ami-03265a0778a880afb"  // ami id of devops-practice at us-east-1
   instance_type = "t2.micro"
   security_groups = ["allow-all"]
-  group_id = ["sg-095cbb3f1f8328ecd"]
-
+ 
   tags = {
     Name = "EC2 instance terraform"
   }
