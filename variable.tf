a84ba8f2db6248a4e3aa8802a6f7f3568463573c
Mounika Potturu <mounika.10669270@ltimindtree.com>
2023-09-09 17:17:53 +0530

    resource creation using variables

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "ami" {
     type = string  # this is the data type
     default= "ami-03265a0778a880afb"  # this is the default value of ur ami_id 
 }
-variable "region"{
+variable "region" {
     default = "us-east-1"
     type = string
 }
@@ -11,15 +11,13 @@ variable "instance_type" {
     default = "t2.micro"
 }
 
-variable "sgname"
-{
+variable "sgname" {
     type = string
     default = "allow-all"
 
 }
 
-variable "cidr_name"
-{
+variable "cidr_name" {
     type = list
     default = ["0.0.0.0/0"]
 }
