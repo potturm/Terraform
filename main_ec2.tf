7bafbd52d25d2a08210feddff84bb9176a683298
Mounika Potturu <mounika.10669270@ltimindtree.com>
2023-09-09 14:03:46 +0530

    edited main.tf

--- main_ec2.tf
+++ main_ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "EC2-Intance" {
   ami           = "ami-03265a0778a880afb"  // ami id of devops-practice at us-east-1
   instance_type = "t2.micro"
-  security_group = "allow-all"
+  security_groups = "allow-all"
   group_id = "sg-095cbb3f1f8328ecd"
 
   tags = {
