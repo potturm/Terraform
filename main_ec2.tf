99fe78bf8566e2583f03fa7e718d744e46b09260
Mounika Potturu <mounika.10669270@ltimindtree.com>
2023-09-09 13:51:49 +0530

    added sg to main.tf|MMM-01

--- main_ec2.tf
+++ main_ec2.tf
@@ -2,8 +2,10 @@
 resource "aws_instance" "EC2-Intance" {
   ami           = "ami-03265a0778a880afb"  // ami id of devops-practice at us-east-1
   instance_type = "t2.micro"
+  security_group = "allow-all"
+  group_id = "sg-095cbb3f1f8328ecd"
 
   tags = {
-    Name = "EC2 inatnce terraform"
+    Name = "EC2 instance terraform"
   }
 }  
